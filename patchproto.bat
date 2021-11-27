7777fb9feafb99c3735e706da61d53eb2a46ec9a
lebarsfa <fabrice.le_bars@ensta-bretagne.org>
2021-11-27 17:17:37 +0100

    Updates from UxVCtrl V390
    Updates from UxVCtrl V390
    
    -
    
    -
    
    Updates from UxVCtrl V390
    
    -
    
    -

--- patchproto.bat
+++ patchproto.bat
@@ -4,8 +4,12 @@
 @sed -i 's/FcnPtr/voidPtr/g' x64\hardwarex_proto.m
 @sed -i 's/s_LIST_ENTRYPtr/voidPtr/g' x86\hardwarex_proto.m
 @sed -i 's/s_LIST_ENTRYPtr/voidPtr/g' x64\hardwarex_proto.m
+@sed -i 's/\x27reserved0\x27, \x27uint8\x232\x27, \x27flags\x27, \x27error\x27/\x27reserved0\x27, \x27uint8\x232\x27, \x27flags\x27, \x27uint8\x27/g' x86\hardwarex_proto.m
+@sed -i 's/\x27reserved0\x27, \x27uint8\x232\x27, \x27flags\x27, \x27error\x27/\x27reserved0\x27, \x27uint8\x232\x27, \x27flags\x27, \x27uint8\x27/g' x64\hardwarex_proto.m
 @sed -i 's/\x27sec\x27, \x27uint8\x27, \x27valid\x27, \x27error\x27/\x27sec\x27, \x27uint8\x27, \x27valid\x27, \x27uint8\x27/g' x86\hardwarex_proto.m
 @sed -i 's/\x27sec\x27, \x27uint8\x27, \x27valid\x27, \x27error\x27/\x27sec\x27, \x27uint8\x27, \x27valid\x27, \x27uint8\x27/g' x64\hardwarex_proto.m
+@sed -i 's/\x27reserved2\x27, \x27uint8\x234\x27, \x27flags\x27, \x27error\x27/\x27reserved2\x27, \x27uint8\x234\x27, \x27flags\x27, \x27uint32\x27/g' x86\hardwarex_proto.m
+@sed -i 's/\x27reserved2\x27, \x27uint8\x234\x27, \x27flags\x27, \x27error\x27/\x27reserved2\x27, \x27uint8\x234\x27, \x27flags\x27, \x27uint32\x27/g' x64\hardwarex_proto.m
 @sed -i 's/\x27gpsFix\x27, \x27uint8\x27, \x27flags\x27, \x27error\x27/\x27gpsFix\x27, \x27uint8\x27, \x27flags\x27, \x27uint8\x27/g' x86\hardwarex_proto.m
 @sed -i 's/\x27gpsFix\x27, \x27uint8\x27, \x27flags\x27, \x27error\x27/\x27gpsFix\x27, \x27uint8\x27, \x27flags\x27, \x27uint8\x27/g' x64\hardwarex_proto.m
 @sed -i 's/\x27fixType\x27, \x27uint8\x27, \x27flags\x27, \x27error\x27, \x27flags2\x27, \x27error\x27/\x27fixType\x27, \x27uint8\x27, \x27flags\x27, \x27uint8\x27, \x27flags2\x27, \x27uint8\x27/g' x86\hardwarex_proto.m
